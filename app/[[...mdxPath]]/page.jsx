95914bb730cfd4ed529fba6dd3e1d11274398eea
Alexander Block <ablock84@gmail.com>
2025-10-31 10:19:20 +0100

    Also update metadata.timestamp with git mod time

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -1,17 +1,46 @@
-import { generateStaticParamsFor, importPage } from 'nextra/pages'
-import { useMDXComponents as getMDXComponents } from '../../mdx-components'
-import {Meta} from "next/dist/lib/metadata/generate/meta";
+import {generateStaticParamsFor, importPage} from 'nextra/pages'
+import {useMDXComponents as getMDXComponents} from '../../mdx-components'
+import {execSync} from "node:child_process";
+import * as fs from "node:fs";
 
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
+function getGitModTime(path) {
+    if (!path) {
+        return undefined
+    }
+
+    try {
+        const allAuthorDates = execSync(
+            `git log --follow --name-status --pretty=format:%aI -- ${path}`,
+        ).toString()
+        const [lastEditExceptPathChangeDate] = allAuthorDates.match(/20[\d-T:.Z+]+$(?!\r?\nR)/m) || []
+        const d = new Date(lastEditExceptPathChangeDate)
+
+        return d.getTime()
+    } catch {
+        const st = fs.statSync(path)
+        return Math.trunc(st.mtimeMs)
+    }
+}
+
+function updateMetadata(metadata) {
+    const modTime = getGitModTime(metadata.filePath)
+
+    if (modTime) {
+        metadata.timestamp = modTime
+
+        // put the gitModTime into <meta> so that we can later use it to generate the sitemap.xml
+        metadata.other = {
+            gitModTime: modTime,
+        }
+    }
+}
+
 export async function generateMetadata(props) {
     const params = await props.params
-    const { metadata } = await importPage(params.mdxPath)
-
-    // put the filePath into <meta> so that we can later use it to determine the git modification time
-    metadata.other = {
-        mdxPath: metadata.filePath
-    }
+    const {metadata} = await importPage(params.mdxPath)
+    updateMetadata(metadata)
     return metadata
 }
 
@@ -20,10 +49,13 @@ const Wrapper = getMDXComponents().wrapper
 export default async function Page(props) {
     const params = await props.params
     const result = await importPage(params.mdxPath)
-    const { default: MDXContent, toc, metadata } = result
+    const {default: MDXContent, toc, metadata} = result
+
+    updateMetadata(metadata)
+
     return (
         <Wrapper toc={toc} metadata={metadata}>
-            <MDXContent {...props} params={params} />
+            <MDXContent {...props} params={params}/>
         </Wrapper>
     )
 }
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 import * as fs from "node:fs";
 
 import * as jsdom from "jsdom";
-import {execSync} from "node:child_process";
 const { JSDOM } = jsdom;
 
 /** @type {import('next-sitemap').IConfig} */
@@ -31,14 +30,10 @@ export default {
                     return null
                 }
 
-                const mdxPath = dom.window.document.querySelector('meta[name="mdxPath"]')?.content
-                if (mdxPath) {
-                    const allAuthorDates = execSync(
-                        `git log --follow --name-status --pretty=format:%aI -- ${mdxPath}`,
-                    ).toString()
-                    const [lastEditExceptPathChangeDate] = allAuthorDates.match(/20[\d-T:.Z+]+$(?!\r?\nR)/m) || []
-                    const d = new Date(lastEditExceptPathChangeDate)
-                    lastmod = d.getTime()
+                // this is generated in page.jsx
+                const gitModTime = dom.window.document.querySelector('meta[name="gitModTime"]')?.content
+                if (gitModTime) {
+                   lastmod = parseInt(gitModTime)
                 }
             } catch (error) {
                 console.error('err', error)
